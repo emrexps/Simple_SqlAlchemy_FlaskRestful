21b3b7b18b5331fe0888bcc0f623d8aa301efb8f
Emre <emre.eker@ymail.com>
2018-09-09 18:53:51 +0300

    post method added

--- Api.py
+++ Api.py
@@ -7,10 +7,22 @@ from flask_restful import reqparse, abort, Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+parser.add_argument('surname')
+
 class Insert(Resource):
     def put(self, name,surname):
         insert(name,surname)
         return "success", 201
+
+"""post method usage..."""
+class Add(Resource):
+    def post(self):
+        args = parser.parse_args()
+        insert(args['name'],args['surname'])
+        return "added sucess",200
+
 
 class ReadSpecific(Resource):
     def get(self,id):
@@ -25,7 +37,7 @@ class ReadAll(Resource):
 api.add_resource(Insert, '/insert/<name>/<surname>')
 api.add_resource(ReadSpecific,'/getbyid/<id>')
 api.add_resource(ReadAll,'/getall')
-
+api.add_resource(Add,'/add')
 
 
 
